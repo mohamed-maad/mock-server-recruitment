6cba9101434cab9286c9942ff4d5b3d8e01183d2
yahya <mohamed.yahya.maad@tietoevry.com>
2024-06-28 04:00:43 +0200

    Needs proper README

--- src/main/kotlin/com/tietoevry/routes/TestRoutes.kt
+++ src/main/kotlin/com/tietoevry/routes/TestRoutes.kt
@@ -11,10 +11,10 @@ fun Route.testRouting(testService: TestService) {
             call.respond("Test for fake token passed: ${testService.testForFakeToken()}")
         }
         get("2") {
-            call.respond("Test for person1: ${testService.testForPerson1()}")
+            call.respond("Test for person1 passed: ${testService.testForPerson1()}")
         }
         get("3") {
-            call.respond("Test for database content: ${testService.testForDatabaseContent()}")
+            call.respond("Test for database content passed: ${testService.testForDatabaseContent()}")
         }
     }
 }
